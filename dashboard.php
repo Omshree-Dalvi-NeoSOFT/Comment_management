d8bbd6fcc885e622c7a8465388152d8bba4b36e3
Omshree-Dalvi-NeoSOFT <omshree.dalvi@neosoftmail.com>
2021-11-04 08:25:49 +0530

    updated

---
+++ dashboard.php
@@ -0,0 +1,54 @@
+<?php 
+ session_start();
+ $sid=$_SESSION['sid'];
+ if(empty($sid)){
+   header("location:index.php");
+ }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+            <!-- Including head tags and other scripting files. -->
+<?php include('head.php')?>     
+</head>
+<body>
+    <?php include('nav.php')?>
+    <section class="mb-3"></section>
+    <section class="container-fluid">
+
+    <section class="row">
+        <section class="col-md-3 ">
+            <?php include('sidebar.php') ?>
+        </section>
+        
+        <section class="col-md-9 ">
+            <!-- open pages on get -->
+           <?php 
+            switch(@$_GET['con']){
+                case 'comment':
+                    include('comment.php');
+                    break;
+                case 'home':
+                    include('post.php');
+                    break;
+                case 'changepass':
+                    include('changepass.php');
+                    break;
+                default:
+                    include('post.php');      
+            }
+           ?> 
+        </section>
+    </section>
+
+    </section>
+    
+    <section class="container-fluid">
+        <!-- include footer file, for page footer -->
+        <?php include('footer.php')?>
+    </section>
+    
+    <?php include('foot.php')?>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,10 @@
+<div class="card">
+  <h5 class="card-header">Featured</h5>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <img src="..." class="card-img-top" alt="...">
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Add Comment</a>
+        <?php include("comment.php");?>
+  </div>
+</div>
